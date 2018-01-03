69fd5eddf1267d452a64860447d6affda7e21b0d
Zach DiUbaldo <zdiubaldo@gmail.com>
2018-01-03 07:18:55 -0700

    added onoff and gpio code to rest endpoints

--- api/controllers/garageController.js
+++ api/controllers/garageController.js
@@ -1,13 +1,22 @@
 'use strict';
+var Gpio = require('onoff').Gpio; 
+var LED = new Gpio(14, 'out'); 
+
 
 exports.door_status = function(req, res) {
-    res.json({ message: 'door is open' });
+  res.json({ message: 'door is open' });
 };
 
 exports.door_close = function(req, res) {
+  if (LED.readSync() === 0) { 
+    LED.writeSync(1);
+        res.json({ message: 'opened the door' });
+  } else {
+    LED.writeSync(0); 
     res.json({ message: 'closed the door' });
+  }
 };
 
 exports.door_open = function(req, res) {
-    res.json({ message: 'opened the door' });
+  res.json({ message: 'opened the door' });
 };
